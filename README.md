e5707ec2d21268300ad79145257368f81e172af3
rkvasisht <rkvasisht@gmail.com>
2018-06-11 03:18:40 -0700

    added style and and images and commented code, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # trivia
-This game will be a two person trivia game.
+This game will be a two person trivia game. The Theme of the game will be based on the 
+popular show the office. The object of the game is score the most points in 30 seconds. 
+Points are scored by answering a question correctly. 10 points are earned on every correct answer.
+The game is a two player game. At the end of two 30 second rounds a winner will be crowned. 
+The player with the most points wins. The players alternate after every round. Player 1 will 
+go first followed by player two. 
+
+Day 1:
+I used a basic css grid in order to make a functional wire map. The Wire only had a set 
+up for 1 question and 4 answers. This day was just used to develop a strategy on how to 
+find possible scenarios on how random questions and answers can be generated from a bank
+of questions and answers. 
+
+Day 2: 
+I developed an array of objects with questions and associated answers. I first randomized index of the array
+and built two separate arrays for the question and the associating answers. I then used the fisher-yates
+algorithm to shuffle the array so the answers would not be in the same position. 
+
+
+Day 3: 
+Now that the arrays of questions and answers where built, I needed to make sure that once a question was asked, 
+it would not be asked again. In order to do this. I used the splice method to remove questions that had be answered, 
+remove them so they would not be asked. I aslo started to integrate a timmer function into my code. Now that I had a set of questions 
+and answers, I then moved on to show if the answers were correct or incorrect. I did this by adding click events with an associated check
+functions which would turn the text green if it was correct and red if it was incorrect. 
+
+Day 4: 
+I worked on troubleshooting the timmer so it would stop after 30 seconds. I was able to solve this problem by make the inner 
+value equal to zero when the value of hit zero. I then started to work on developing on what functions and parameters would be set 
+during different time periods. An example of this is, when the timer hits zero seconds, a final score should by shown. 
+
+Day 5: 
+
+I finalized all of my time periods for player 1 and player 2 and then moved on to the win loss logic. I developed a set of if statements
+depending on the score. I created a win scenario for player 1, player 2 and a tie scenario. This was done by comparing the final score of player two, 
+vs. the score to beat(which was equal to the score of player 1). I spent this day also developing a reset function and taking care of some 
+bugs in the code. 
+
+Day 6: 
+
+This day was spent on working through the styling of the game. I went through multiple different styles but a lot of them didn't represent the 
+theme of the game.
+
+Day 7:
+
+Once I created a theme, I was able to style it to look like an office cork board. I used a series of css grid techniques in order to keep 
+the images and text inline. I added a function that switched the picture of the players after during their turn. This day was challenging for 
+me as I am not as strong in styling but I think the project came out pretty well. 
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
 var master=[
 
-		{'question':'what color is the sky?',
-	 	'answer':'blue',
-	 	'option1':'green',
-	 	'option2':'brown',
-	 	'option3':'purple'
+		{'question':'Which holiday did Pam retunr on after the birth of her second child?',
+	 	'answer':'Christmas',
+	 	'option1':'Arbor Day',
+	 	'option2':'Columbus Day',
+	 	'option3':'Valentines day'
 		},
 
-		{'question':'what is 2+2?',
-	 	'answer':'4',
-	 	'option1':'3',
-	 	'option2':'7',
-	 	'option3':'97'
+		{'question':'Diangelo Vickers loves what region of the United States?',
+	 	'answer':'Southwest',
+	 	'option1':'Plain States',
+	 	'option2':'Pacific Northwest',
+	 	'option3':'New England'
 		},
 
-		{'question':'what is my name',
+		{'question':'Darly turned down the Print in colors ',
 	 	'answer':'rahul',
 	 	'option1':'bob',
 	 	'option2':'moe',
@@ -88,44 +88,69 @@ var master=[
 
 
 	];
+//question bank
 var qArray=master;
+// array of quesitons 
 var questionRandom = [];
+// array of random quesiont
 var correct = [];
+// array of correct answers
 var incorrect1 = [];
+// array of possible answers
 var incorrect2 = [];
+// array of possible answer
 var incorrect3 = [];
+// array of possible answers
 var a = [];
+// array of correct, and all 3 inncorrect answers.
 var foo = [];
+// array of answers shuffled
 var fooTwo = [];
+// array of questions shuffled 
 var scoreOne = 0;
+// player one score
 var scoreTwo = 0;
+// player two score
 var remainQ = [];
-var startTime = 11; 
+// array of remaining questions after the splice of used questions
+var startTime = 30; 
+// starting time of timer. 
 var test = null;
+// set an interval of 1 second to the timer function
 var player =[0,1];
+// array that designated the players. 0=player 1 and 1= player 2. 
 var turn = 0;
+// represents the index of the player array. 0 starts on player 1. 
 var counter = 0;
+// counter counts how many clicks have been made on the start button. 
 var scoreWin = 0;
+// used to determine the winning score. 
 var canClick = true;
+// variable used to deactivate clicking events during timeouts. 
 
 
 
 var initialize = function(){
-
+//function used to fill out the question and answer section after every click. 
   
-console.log(qArray.length)
+
 var rando = Math.floor(Math.random()* qArray.length)
+// used to get a random index of the qArray
  questionRandom = qArray[rando];
-console.log(questionRandom);
+// used to keep track of the questions that have been asked. 
+
  correct = questionRandom.answer;
-console.log(correct);
+// this is the answer to the question that is being asked.
  incorrect1 = questionRandom.option1;
-console.log(incorrect1);
+ // this is the possible answer to the question that is being asked. 
+
  incorrect2 = questionRandom.option2;
  incorrect3 = questionRandom.option3;
  qArray.splice(rando, 1)
- console.log('post splice', qArray)
+ //this spliced the question being asked out of the question array so it can't be asked again. 
+ 
  a =[correct, incorrect1, incorrect2, incorrect3];
+
 
 	Array.prototype.shuffle = function() {
 	    var input = this;
@@ -140,12 +165,14 @@ console.log(incorrect1);
 	    }
 	    return input;
 	}
-
+//this function shuffles the answer array 
  foo = a.shuffle();
-console.log(foo);
+// this is result of the shuffled array 
+
+
 
  fooTwo = questionRandom.question;
-console.log(fooTwo);
+// this is the result of the shuffled question
 	
 	document.getElementById("questionAsked").innerText=fooTwo;
 	document.getElementById("answerOne").innerText=foo[0];
@@ -153,11 +180,14 @@ console.log(fooTwo);
 	document.getElementById("answerThree").innerText=foo[2];
 	document.getElementById("answerFour").innerText=foo[3];
 
-
+//the inner text of the question and answer divs are set here. 
 
 };
 
+
+
 function startOver(){
+//reset parameters
 	qArray = master;
 	 questionRandom = [];
 	 correct = [];
@@ -189,167 +219,202 @@ function startOver(){
 	
 
  check1 =function(){
+ //check fuction for answer1 div
 	if(document.getElementById("answerOne").innerText==correct){
 		document.getElementById("answerOne").style.color = "green";
+		//change text if green. 
 		if(player[turn]==0){
 		scoreOne += 10;
+		//adding 10 points to player 1 score
 		scoreWin += 10;
+		//adding 10 points to keep track of player 1 score(this might look like a redundantcy but
+		//but kept in for expansion of more than 2 players. 
 		document.getElementById("score").innerText = scoreOne;
-		console.log("you're right")
+		//puts score in div
 		}
 		if(player[turn]==1){
 		scoreTwo += 10;
+		//player 2 scoring
 		document.getElementById("score").innerText = scoreTwo;
-		console.log("you're right")
+		
 		}
 
 	}
 	else{
 	document.getElementById("answerOne").style.color = "red";
-	console.log("you're wrong!")
+	//change text if wrong.
 	}
 };
 
  check2 = function(){
+ 	//check two for div 2. Same functionality as check 1.
 	if(document.getElementById("answerTwo").innerText==correct){
 		document.getElementById("answerTwo").style.color = "green";
 		if(player[turn]==0){
 		scoreOne += 10;
 		scoreWin += 10;
 		document.getElementById("score").innerText = scoreOne;
-		console.log("you're right")
+		
 		}
 		if(player[turn]==1){
 		scoreTwo += 10;
 		document.getElementById("score").innerText = scoreTwo;
-		console.log("you're right")
+		
 		}
 	}
 	else{
 	document.getElementById("answerTwo").style.color = "red";
-	console.log("you're wrong!")
+	
 	}
 };
 
  check3 = function(){
+ 	//check two for div 3. Same functionality as check 1.
 	if(document.getElementById("answerThree").innerText==correct){
 		document.getElementById("answerThree").style.color = "green";
 		if(player[turn]==0){
 		scoreOne += 10;
 		scoreWin += 10;
 		document.getElementById("score").innerText = scoreOne;
-		console.log("you're right")
+		
 		}
 		if(player[turn]==1){
 		scoreTwo += 10;
 		document.getElementById("score").innerText = scoreTwo;
-		console.log("you're right")
+		
 		}
 	}
 	else{
 	document.getElementById("answerThree").style.color = "red";
-	console.log("you're wrong!")
+	
 	}
 };
 
  check4 = function(){
+ 	//check two for div 4. Same functionality as check 1.
 	if(document.getElementById("answerFour").innerText==correct){
 		document.getElementById("answerFour").style.color = "green";
 		if(player[turn]==0){
 		scoreOne += 10;
 		scoreWin += 10;
 		document.getElementById("score").innerText = scoreOne;
-		console.log("you're right")
+	
 		}
 		if(player[turn]==1){
 		scoreTwo += 10;
 		scoreWin += 0
 		document.getElementById("score").innerText = scoreTwo;
-		console.log("you're right")
+		
 		}
 	}
 	else{
 	document.getElementById("answerFour").style.color = "red";
-	console.log("you're wrong!")
-	console.log(correct);
+	
 	}
 };
 
 var reset = function(){
-
+//fucntion resets the color of the all the answers back to black after every click. 
 document.getElementById("answerOne").style.color = "black";
 document.getElementById("answerTwo").style.color = "black";
 document.getElementById("answerThree").style.color = "black";
 document.getElementById("answerFour").style.color = "black";
 	
 	initialize();
-
+//calls the initialize function so another question can be asked. 
 
 
 }
 
 var slowReset = function(){
-
+//added a 700 mil sec delay to the reset function so the right or wrong answer will be shown first then the new q and a with show. 
 	setTimeout(reset, 700);
 };
 
 function stop(){
-
+//this function is to stop the time interval. 
 	clearInterval(test);
 };
 
 function counterReset(){
+//this function reset parameters for the second player to take their turn. 
 	if(counter>1){
-		startTime = 11;
+		startTime = 30;
 		scoreOne = 0;
 		turn = 1;
 		canClick = true;
+		document.getElementById("score").innerText="Score"
+		playerId();
+		//this function changes the player name tag. 
 
 
 		
 	}
 };
+function playerId(){
+	//this function changes the player name tag. 
+	if(turn==0){
+		document.getElementById("start").style.backgroundImage = "url('images/dwight.jpg')";
+	}
+	else{
+		document.getElementById("start").style.backgroundImage = "url('images/jim.jpg')";
+	}
+}
 
 function timerBox(){
-
+//timing function
 	if(counter==1){
+		//function and paramenters are set after the first click. During player ones turn 
 		startTime--;
 		document.getElementById("start").innerText="Game in Progress";
 		if (startTime == 0){
+			//when the timer hits zero the following things paraments and function take place.
 			canClick = false;
+			//click events can't be clicked
 			startTime = 0;
 			stop();
+			//interval is cleard.
 			document.getElementById("scoreToBeat").innerText="Score to beat:"+scoreWin;
 			document.getElementById("score").innerText="final score:"+scoreOne;
 			document.getElementById("start").innerText="Player 2 click to start";
 			document.getElementById("timer").innerText="Times up!"
-			
+			//final score for player one are shown 
 		}
 		else{
 		document.getElementById("timer").textContent = "0:" + startTime;
+		//the timer will keep running until it hits zero
 		}
 
 	}
 	if(counter>1){
-		
+	//for the second click and the start of player 2's turn, the following parameters 
+	//are put in place.	
 		
 		startTime--;
+		//the start time goes down by 1
 		document.getElementById("start").innerText="Game in Progress";
+		//game in progress is shown while player two is playing 
 		if (startTime == 0){
+		//paraments after player 2 time runs out. 
 			canClick = false;
+			//click events for answers don't work 
 			startTime = 0;
 			stop();
 			document.getElementById("score").innerText="final score:"+scoreTwo;
 			document.getElementById("start").innerText="Game Over!";
 			document.getElementById("timer").innerText="Times up!";
-			console.log(scoreWin,scoreTwo);
+
+			
 			if(scoreWin > scoreTwo){
-				document.getElementById("player").innerText = "Player 1 Wins! Click here to play again";
+				document.getElementById("player").innerText = "And the Dundie goes to player 1! Click here to play again";
 			}
+			//win condition for player 1 win based off of if scoreWin is greater than player 2 score.
 			if(scoreWin < scoreTwo){
-				document.getElementById("player").innerText = "Player 2 Wins! Click here to play again";
+				document.getElementById("player").innerText = "And the Dundie goes to player 2! Click here to play again";
 			}
+			//win condition for player 2 win based off of if scoreWin is less than player 2 score.
 			if(scoreWin === scoreTwo){
+			//win condition for tie based off of if scoreWin equals player 2 score.
 				document.getElementById("player").innerText = "It's a Tie! Click here to play again"
 			}
 		}
@@ -363,9 +428,11 @@ function timerBox(){
 
 var clickStart = document.getElementById("start");
 clickStart.addEventListener('click', function(){
-	
+//click event for the start function. 	
 	counter++;
+	//counter for keeping track of how many clicks are made. 
 	counterReset();
+
 	test = setInterval(timerBox,1000);
 	initialize();
 	
@@ -380,7 +447,7 @@ clickAOne.addEventListener('click', function(){
 		slowReset();
 	
 	}
-	
+//click event for answer1 div. Also defines when click can be made. 	
 }); 
 
 
@@ -392,9 +459,8 @@ clickATwo.addEventListener('click', function(){
 	
 
 	}
-
+//click event for answer2 div. Also defines when click can be made. 
 }); 
-
 
 
 
@@ -405,6 +471,7 @@ clickAThree.addEventListener('click', function(){
 	slowReset();
 
 	};
+//click event for answer3 div. Also defines when click can be made. 
 });
 
 
@@ -414,45 +481,15 @@ clickAFour.addEventListener('click', function(){
 	if(canClick){
 	check4();
 	slowReset();
-	
+//click event for answer2 div. Also defines when click can be made. 	
 	};
 });
 
 var clickReset = document.getElementById("player");
 clickReset.addEventListener('click', startOver);
+// click event for reseting the game. 
 
 
 
-	// if()
-	// startTime--;
-	// if (startTime <= 0){
-	// startTime = 0;
-	// stop();
-	// document.getElementById("scoreToBeat").innerText=player[0];
-	// document.getElementById("score").innerText="final score:"+player[turn];
-	// turn=1;
-	// if(counter==1){
-		
-	// 	document.getElementById("score").innerText=0;
-	// 	document.getElementById("start").innerText ="Player 2 Start"
-	// 	startTime = 31;
-	// 	startTime--;
-	// 	if (startTime <= 0){
-	// 		startTime = 0;
-	// 		stop();
-	// 		if(player[0] > player[turn]){
-	// 			console.log("player 1 wins")
-	// 		}
-	// 		else{
-	// 			console.log("player 2 wins")
-	// 		};
 
-	// 	};
-
-	// }
-
-
-	// }
-	// document.getElementById("timer").textContent = "0:" + startTime;
-
-
+	
